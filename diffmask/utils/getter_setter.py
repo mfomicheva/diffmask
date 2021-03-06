7d6985cd08cc339a1bf82ba44fc2b60969a5542c
m_fomicheva <mari.fomicheva@gmail.com>
2021-03-06 20:28:40 +0100

    Added roberta setter

--- diffmask/utils/getter_setter.py
+++ diffmask/utils/getter_setter.py
@@ -3,6 +3,60 @@ from transformers import BertForSequenceClassification
 from collections import defaultdict
 
 
+def roberta_setter(model, inputs_dict, hidden_states, forward_fn=None):
+
+    hidden_states_ = []
+
+    def get_hook(i):
+        def hook(module, inputs, outputs=None):
+            if i == 0:
+                if hidden_states[i] is not None:
+                    hidden_states_.append(hidden_states[i])
+                    return hidden_states[i]
+                else:
+                    hidden_states_.append(outputs)
+
+            elif 1 <= i <= len(model.roberta.encoder.layer):
+                if hidden_states[i] is not None:
+                    hidden_states_.append(hidden_states[i])
+                    return (hidden_states[i],) + inputs[1:]
+                else:
+                    hidden_states_.append(inputs[0])
+
+            elif i == len(model.roberta.encoder.layer) + 1:
+                if hidden_states[i] is not None:
+                    hidden_states_.append(hidden_states[i])
+                    return (hidden_states[i],) + outputs[1:]
+                else:
+                    hidden_states_.append(outputs[0])
+
+        return hook
+
+    handles = (
+        [model.roberta.embeddings.word_embeddings.register_forward_hook(get_hook(0))]
+        + [
+            layer.register_forward_pre_hook(get_hook(i + 1))
+            for i, layer in enumerate(model.roberta.encoder.layer)
+        ]
+        + [
+            model.roberta.encoder.layer[-1].register_forward_hook(
+                get_hook(len(model.roberta.encoder.layer) + 1)
+            )
+        ]
+    )
+
+    try:
+        if forward_fn is None:
+            outputs = model(**inputs_dict)
+        else:
+            outputs = forward_fn(**inputs_dict)
+    finally:
+        for handle in handles:
+            handle.remove()
+
+    return outputs, tuple(hidden_states_)
+
+
 def roberta_getter(model, inputs_dict, forward_fn=None):
 
     hidden_states_ = []
