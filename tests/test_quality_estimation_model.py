08acd17b77c03f6edd594bae5ceeff40618fbc8e
m_fomicheva <mari.fomicheva@gmail.com>
2021-04-12 12:47:22 +0200

    Pass file_prefix

--- tests/test_quality_estimation_model.py
+++ tests/test_quality_estimation_model.py
@@ -13,15 +13,15 @@ class TestQualityEstimationModel(unittest.TestCase):
     def test_prepare_data(self):
         bert_model = "xlm-roberta-base"
         tokenizer = XLMRobertaTokenizer.from_pretrained(bert_model)
-
+        file_prefix = 'test'
         with tempfile.TemporaryDirectory("test_prepare_data") as data_dir:
-            create_dummy_data(data_dir)
+            create_dummy_data(data_dir, file_prefix)
             dataset = load_sent_level(
-                os.path.join(data_dir, 'src'),
-                os.path.join(data_dir, 'tgt'),
-                os.path.join(data_dir, 'labels'),
+                os.path.join(data_dir, f'{file_prefix}.src'),
+                os.path.join(data_dir, f'{file_prefix}.tgt'),
+                os.path.join(data_dir, f'{file_prefix}.labels'),
                 tokenizer,
-                path_word_labels=os.path.join(data_dir, 'tags')
+                path_word_labels=os.path.join(data_dir, f'{file_prefix}.tags')
             )
             assert dataset[0][1][0].shape[0] == 128
             assert sum(dataset[0][1][1]).item() == 20
--- tests/util.py
+++ tests/util.py
@@ -9,13 +9,12 @@ random.seed(1234)
 vocab = ['es', 'una', 'frase', 'con', 'muchas', 'palabras', 'this', 'is', 'a', 'sentence', 'with', 'many', 'words']
 
 
-def create_dummy_data(data_dir, num_examples=100, max_len=100):
-    data = torch.randint(len(vocab), (num_examples * max_len * 2,))
+def _make_files(data, num_examples, max_len, data_dir, file_prefix):
     offset = 0
-    with open(os.path.join(data_dir, 'src'), 'w') as s:
-        with open(os.path.join(data_dir, 'tgt'), 'w') as t:
-            with open(os.path.join(data_dir, 'labels'), 'w') as l:
-                with open(os.path.join(data_dir, 'tags'), 'w') as w:
+    with open(os.path.join(data_dir, f'{file_prefix}.src'), 'w') as s:
+        with open(os.path.join(data_dir, f'{file_prefix}.tgt'), 'w') as t:
+            with open(os.path.join(data_dir, f'{file_prefix}.labels'), 'w') as l:
+                with open(os.path.join(data_dir, f'{file_prefix}.tags'), 'w') as w:
                     for _ in range(num_examples):
                         for fh in (s, t):
                             ex_len = random.randint(1, max_len + 1)
@@ -23,3 +22,8 @@ def create_dummy_data(data_dir, num_examples=100, max_len=100):
                             offset += ex_len
                         print(torch.randint(2, (1,)).item(), file=l)
                         print(' '.join(map(str, torch.randint(2, (ex_len,)).tolist())), file=w)
+
+
+def create_dummy_data(data_dir, file_prefix, num_examples=100, max_len=100):
+    data = torch.randint(len(vocab), (num_examples * max_len * 2,))
+    _make_files(data, num_examples, max_len, data_dir, file_prefix)
