fb73f24101049c62453174bf92d55892c50fb9c1
m_fomicheva <mari.fomicheva@gmail.com>
2021-04-20 21:53:31 +0200

    Removed print

--- diffmask/attributions/schulz.py
+++ diffmask/attributions/schulz.py
@@ -47,7 +47,6 @@ def schulz_explainer(
         requires_grad=True,
         device=next(model.parameters()).device,
     )
-    print(alpha.shape)
     optimizer = torch.optim.RMSprop([alpha], lr=lr, centered=True)
 
     t = trange(steps) if verbose else range(steps)
