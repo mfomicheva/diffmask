3aebcdb7ec97c58dc6cd65c525b9a05079b50f17
m_fomicheva <mari.fomicheva@gmail.com>
2021-04-21 15:34:23 +0200

    Pass weight decay as parameter

--- diffmask/models/quality_estimation.py
+++ diffmask/models/quality_estimation.py
@@ -177,7 +177,8 @@ class QualityEstimation(pl.LightningModule):
     def configure_optimizers(self):
         total = len(self.train_dataloader()) * self.hparams.epochs
         warmup_steps = math.ceil(total * self.hparams.warmup_percentage)
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.hparams.learning_rate, eps=1e-8)
+        optimizer = torch.optim.AdamW(
+            self.parameters(), lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
         scheduler = get_linear_schedule_with_warmup(
             optimizer, num_warmup_steps=warmup_steps, num_training_steps=total
         )
--- diffmask/options.py
+++ diffmask/options.py
@@ -27,6 +27,7 @@ def make_parser():
     parser.add_argument("--val_loss", default="f1", choices=["f1", "mcc", "mse", "pearson"])
     parser.add_argument("--num_labels", default=2, type=int)
     parser.add_argument("--target_only", action='store_true', default=False)
-    parser.add_argument("--clip_grad", default=0., type=float)
+    parser.add_argument("--clip_grad", default=1., type=float)
     parser.add_argument("--warmup_percentage", default=0., type=float, help="Percentage of iterations for warmup")
+    parser.add_argument("--weight_decay", default=0., type=float,)
     return parser
