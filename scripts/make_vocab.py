249d76f60d4805e6e2b97e38d889a680f7d58fee
m_fomicheva <mari.fomicheva@gmail.com>
2021-04-23 13:30:54 +0200

    Add optional minimum frequency threshold

--- scripts/make_vocab.py
+++ scripts/make_vocab.py
@@ -10,6 +10,7 @@ parser.add_argument('-i', '--infile')
 parser.add_argument('-o', '--outfile')
 parser.add_argument('-l', '--lang')
 parser.add_argument('--tokenize', default=False, action='store_true')
+parser.add_argument('--min_freq', default=1, type=int)
 args = parser.parse_args()
 
 tokenizer = MosesTokenizer(lang=args.lang)
@@ -25,5 +26,7 @@ for line in open(args.infile):
 print('Writing vocabulary with counts...')
 with open(args.outfile, 'w') as o:
     for token in sorted(result, key=lambda c: c[1], reverse=True):
+        if result[token] < args.min_freq:
+            break
         o.write('{}\t{}\n'.format(token, result[token]))
 o.close()
